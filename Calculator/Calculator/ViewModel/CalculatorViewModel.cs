369ccea60a0de3c2ddd76df24ca056081623a086
Borislav <boby.filipov@mail.bg>
2016-05-04 01:15:42 +0300

    asd

---
+++ Calculator/Calculator/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
+{
+    class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void PropChanged([CallerMemberName]String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private string _tbEquation;
+        public string tbEquation
+        {
+            get
+            {
+                return _tbEquation;
+            }
+            set
+            {
+                _tbEquation = value;
+                PropChanged();
+            }
+        }
+        private ICommand _btnCommand;
+        public ICommand BtnCommand
+        {
+            get
+            {
+                return _btnCommand;
+            }
+            set
+            {
+                _btnCommand = value;
+            }
+        }
+        public void appendEquation(object obj)
+        {
+            tbEquation += obj.ToString();
+        }
+        public CalculatorViewModel()
+        {
+            BtnCommand = new DelegateCommand(appendEquation);
+        }
+    }   
+}
---
+++ Calculator/Calculator/ViewModel/DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
+{
+    class DelegateCommand : ICommand
+    {
+        private Action<object> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            this.execute = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public void Destroy()
+        {
+            this.execute = _ => { return; };
+        }
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
